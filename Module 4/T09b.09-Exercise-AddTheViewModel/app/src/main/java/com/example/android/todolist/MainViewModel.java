37d20fb169e69ec25eed40b457ae967edd8dbd13
muralimandula <murali.mandula@msitprogram.net>
2019-03-19 15:56:29 +0530

    M4 A54 - Adding the View Model

---
+++ Module 4/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -0,0 +1,31 @@
+package com.example.android.todolist;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
+
+import java.util.List;
+
+// TODO (1) make this class extend AndroidViewModel and implement its default constructor
+public class MainViewModel extends AndroidViewModel {
+
+    // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+    private LiveData<List<TaskEntry>> taskMember;
+
+    // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+    public MainViewModel(Application application) {
+        super(application);
+
+        AppDatabase db = AppDatabase.getInstance(this.getApplication());
+        taskMember = db.taskDao().loadAllTasks();
+    }
+
+    // TODO (3) Create a getter for the tasks variable
+
+    public LiveData<List<TaskEntry>> getTasks(){
+        return taskMember;
+    }
+}
